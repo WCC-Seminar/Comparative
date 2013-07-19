0af774219616db8c5d6c8230257aa60aae3a32bf
exumbra_insolem <a.harmonyroad@gmail.com>
2013-07-20 02:44:59 +0900

    Hello World

---
+++ HelloWorld/HelloWorld.hs
@@ -0,0 +1,4 @@
+hello1 = putStr $ "Hello, World!\n"
+-- hello2 = putStrLn $ "Hello, World!"
+main = hello1
+-- in Haskell, I/O is quite tricky inside.
---
+++ HelloWorld/HelloWorld.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+def main():
+    print "Hello, World!"
+# in python3.x, 
+# print ("Hello, World!")
+
+if __name__ == "__main__":
+    main()
