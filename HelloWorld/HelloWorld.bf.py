1bf47a77595b0ad4c24611ab728359419747996d
exumbra_insolem <a.harmonyroad@gmail.com>
2013-07-23 14:25:20 +0900

    Hello World, brainsth implementaion written in python. added the ability to visualise the execution.

--- HelloWorld/HelloWorld.bf.py
+++ HelloWorld/HelloWorld.bf.py
@@ -2,11 +2,13 @@
 import sys
 
 class Brainsth(object):
-    def __init__(self, n):
+    def __init__(self, n, doesprint = True):
         self.tapelength = n
         self.tape = [0] * n
         self.pointer = 0
         self.reader = 0
+        self.printed_chars = ""
+        self.doesprint = doesprint
         self.right = '>'
         self.left  = '<'
         self.incr  = '+'
@@ -20,6 +22,7 @@ class Brainsth(object):
         self.tape = [0] * self.tapelength
         self.pointer = 0
         self.reader = 0
+        self.printed_chars = ""
 
     def execute(self,command):
         self.refresh()
@@ -29,6 +32,11 @@ class Brainsth(object):
                 currentcommand = command[self.reader]
             except:
                 break
+
+            if self.doesprint:
+                self.printtape()
+                self.printcommands(command)
+                self.reprint()
 
             if currentcommand == self.right:
                 self.pointer += 1
@@ -40,6 +48,9 @@ class Brainsth(object):
                 self.tape[self.pointer] -= 1
             elif currentcommand == self.output:
                 sys.stdout.write(chr(self.tape[self.pointer]))
+                if self.doesprint:
+                    sys.stdout.write('\n')
+                    self.printed_chars += chr(self.tape[self.pointer])
             elif currentcommand == self.accept:
                 self.tape[self.pointer] = input('\n')
             elif currentcommand == self.whileb:
@@ -51,7 +62,34 @@ class Brainsth(object):
             else:
                 pass
             self.reader += 1
+
+        if self.doesprint:
+            self.reprint()
         return 0
+    
+    def emph(self,chars):
+        sys.stdout.write("\033[0;31m%s\033[0m" %(chars))
+
+    def printtape(self):
+        for x in self.tape[:self.pointer]:
+            sys.stdout.write("%d\t" %(x))
+        self.emph(self.tape[self.pointer])
+        sys.stdout.write("\t")
+        for x in self.tape[self.pointer +1 :]:
+            sys.stdout.write("%d\t"%(x))
+        sys.stdout.write('\n')
+
+    def printcommands(self,command):
+        for c in command[:self.reader]:
+            sys.stdout.write("%s" %(c))
+        self.emph(command[self.reader])
+        for c in command[self.reader+1:]:
+            sys.stdout.write("%s" %(c))
+        sys.stdout.write('\n')
+
+    def reprint(self):
+        print self.printed_chars
+
 
 
 def hello_world():
