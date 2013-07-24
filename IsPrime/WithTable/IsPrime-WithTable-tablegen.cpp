caa5ee6921110bb7cb53d782ed69e494536b36c8
y-serine <serine.in.sericin@gmail.com>
2013-07-25 00:41:38 +0900

    コメントアウトするとこ間違えてたー
    あと、ビット演算子から割り算への変更は見やすくするため。
    たしかunsignedでは速さは同じだったので。

--- IsPrime/WithTable/IsPrime-WithTable-tablegen.cpp
+++ IsPrime/WithTable/IsPrime-WithTable-tablegen.cpp
@@ -1,34 +1,32 @@
-//#include<stdio.h>
 #include<bitset>
 #define NUMMAX 4294967294
-//2147483648
-
 
 void calc(void){	
 	FILE* fp;
-	unsigned long cnt,p, JMAX;
+	unsigned long /*cnt,*/p, JMAX;
 	fopen_s(&fp,"primes.dat","wb");
 	std::bitset<NUMMAX/2+1> num;
-	JMAX=(NUMMAX+1)>>1;
+	JMAX=(NUMMAX+1)/2;
 	num.set();
 	unsigned long two=2;
-	//fwrite(&two,sizeof(unsigned long),1,fp);
-	cnt=1;
+	fwrite(&two,sizeof(unsigned long),1,fp);
+	//cnt=1;
 	for(unsigned long long i=3;i<=NUMMAX;i+=2){
-		p=i>>1;
+		p=i/2;
 		if(num[p]){
-			cnt++;
-			//fwrite(&i,sizeof(unsigned long),1,fp);
+			//cnt++;
+			fwrite(&i,sizeof(unsigned long),1,fp);
 			for(unsigned long long j=p;j<JMAX;j+=i){
 				num.reset(j);
 			}
 		}
 	}
-
+/*
 	fclose(fp);
 	fopen_s(&fp,"count.dat","wb");
 	fwrite(&cnt,sizeof(long),1,fp);
 	fclose(fp);
+*/
 }
 
 int main(void){
