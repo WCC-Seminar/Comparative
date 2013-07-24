8174ebc8380bf7a5498bd6088654ca48f53d25c5
y-serine <serine.in.sericin@gmail.com>
2013-07-25 00:32:09 +0900

    更新忘れ
    素数表作るやつ更新するの忘れてた

--- IsPrime/WithTable/IsPrime-WithTable-tablegen.cpp
+++ IsPrime/WithTable/IsPrime-WithTable-tablegen.cpp
@@ -1,33 +1,34 @@
 //#include<stdio.h>
 #include<bitset>
-#define NUMMAX 2147483648
+#define NUMMAX 4294967294
+//2147483648
 
-void calc(void){  
+
+void calc(void){	
 	FILE* fp;
-	long cnt,JMAX;
+	unsigned long cnt,p, JMAX;
 	fopen_s(&fp,"primes.dat","wb");
 	std::bitset<NUMMAX/2+1> num;
 	JMAX=(NUMMAX+1)>>1;
 	num.set();
-	long two=2;
-	fwrite(&two,sizeof(long),1,fp);
-	//cnt=1;
-	for(long long i=3;i<=NUMMAX;i+=2){
-		if(num[i>>1]){
-			//cnt++;
-			fwrite(&i,sizeof(long),1,fp);
-			for(long j=i>>1;j<JMAX;j+=i){
+	unsigned long two=2;
+	//fwrite(&two,sizeof(unsigned long),1,fp);
+	cnt=1;
+	for(unsigned long long i=3;i<=NUMMAX;i+=2){
+		p=i>>1;
+		if(num[p]){
+			cnt++;
+			//fwrite(&i,sizeof(unsigned long),1,fp);
+			for(unsigned long long j=p;j<JMAX;j+=i){
 				num.reset(j);
 			}
 		}
 	}
 
 	fclose(fp);
-	/*
 	fopen_s(&fp,"count.dat","wb");
 	fwrite(&cnt,sizeof(long),1,fp);
 	fclose(fp);
-	*/
 }
 
 int main(void){
